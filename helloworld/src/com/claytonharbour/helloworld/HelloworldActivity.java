29bc12b9bd21161d564b37fc5052fe02896cb69b
Clayton Harbour <claytonharbour@gmail.com>
2012-02-21 11:00:25 -0800

    added "foo"

--- helloworld/src/com/claytonharbour/helloworld/HelloworldActivity.java
+++ helloworld/src/com/claytonharbour/helloworld/HelloworldActivity.java
@@ -2,6 +2,7 @@ package com.claytonharbour.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloworldActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,9 @@ public class HelloworldActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        TextView tv = new TextView(this);
+        tv.setText("foo");
+        setContentView(tv);
     }
 }
